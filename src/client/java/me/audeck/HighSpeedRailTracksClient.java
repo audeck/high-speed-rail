eb87aefb16e36d9d0c541c944fd3e0dfd7539af8
Audeck <55060124+Audeck@users.noreply.github.com>
2024-11-08 18:32:59 +0100

    Initial commit

---
+++ src/client/java/me/audeck/HighSpeedRailTracksClient.java
@@ -0,0 +1,10 @@
+package me.audeck;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HighSpeedRailTracksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
